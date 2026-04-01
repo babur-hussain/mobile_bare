2ea40c0ddd9b27dccbab389b11fff0b4e5cfaad2
baburhussain <baburhussain660@gmail.com>
2026-04-02 01:30:50 +0530

    Fix Google Sign-In setup: update webClientId and debug SHA1

--- App.tsx
+++ App.tsx
@@ -7,7 +7,9 @@ import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 GoogleSignin.configure({
   webClientId:
-    '208100625363-5gb0dd86nvgkmbpifqe41142rq7l2h6b.apps.googleusercontent.com', // Extracted from iOS GoogleService-Info.plist for now
+    '208100625363-gq9qa2rgvnphdq4cls7ecpq7cs1dl371.apps.googleusercontent.com', // Web Client ID from google-services.json
+  iosClientId:
+    '208100625363-5gb0dd86nvgkmbpifqe41142rq7l2h6b.apps.googleusercontent.com', // Extracted from iOS GoogleService-Info.plist
 });
 
 function App() {
